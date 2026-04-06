fd063705f47d96305155ef43a1fd5f27f3a66f40
llanabanana <mirkov.sv23.2023@uns.ac.rs>
2026-04-06 22:03:40 +0200

    Enhance Candidate and Skill services with error handling and refactor repository methods

--- src/main/java/com/example/demo/repository/CandidateRepository.java
+++ src/main/java/com/example/demo/repository/CandidateRepository.java
@@ -1,8 +1,21 @@
 package com.example.demo.repository;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import com.example.demo.model.Candidate;
 
 public interface CandidateRepository extends JpaRepository<Candidate, Long> {
+
+    @Override
+    @EntityGraph(attributePaths = "skills")
+    Optional<Candidate> findById(Long id);
+
+    @Override
+    @EntityGraph(attributePaths = "skills")
+    List<Candidate> findAll();
 
 }
